f1577df579f14fbe048b10ccfc5f2bd4e800c2a3
void4 <void4@users.noreply.github.com>
2017-09-17 21:07:58 +0200

    Selbstständige und Beamte hinzugefügt

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 f = open("data.txt")
+f2 = open("customdata.txt")
 from collections import OrderedDict
 import json
 
@@ -7,7 +8,7 @@ cats = {"name":"flare", "children":[]}
 def idlen(a):
     return len(a.split(" ")[0])
 
-lines = sorted([line.strip() for line in f.readlines()], key=idlen)
+lines = sorted([line.strip() for line in f.readlines()+f2.readlines()], key=idlen)
 
 def clearnum(s):
     num = s.replace(",","").replace("*","")
@@ -29,7 +30,11 @@ for line in lines:
             root = key["children"]
         if len(cid)==4:
             num = clearnum(line[1])
-            root.append({"name":line[0].strip(), "size":num, "males":float(clearnum(line[2]))/num if num else 0.5})
+            if num and len(line)>2:
+                males = float(clearnum(line[2]))/num
+            else:
+                males = 0.5
+            root.append({"name":line[0].strip(), "size":num, "males":males})
         else:
             root.append({"name":line[0].strip(), "children":[]})
     else:
